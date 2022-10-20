2283243361908bc2f99d276922ef32184e1b3d75
João Vitor Machado <77728952+joovitor12@users.noreply.github.com>
2022-10-20 09:52:23 -0300

    refactor

--- AlgoritmoGenetico/src/Main.java
+++ AlgoritmoGenetico/src/Main.java
@@ -3,37 +3,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		//Define a solução -> maior que 5
-		Scanner in = new Scanner(System.in);
-		System.out.print("Defina a solucao: ");
-		String solucao = in.nextLine();
-		while(solucao.length() < 5) {
-			System.out.println("Tamanho invalido, insira uma String com tamanho maior que 5:");
-			solucao = in.nextLine();
-		}
 		
-        AG.setSolucao(solucao);    
-        AG.setCaracteres("!,.:;?áÁãÃâÂõÕôÔóÓéêÉÊíQWERTYUIOPASDFGHJKLÇZXCVBNMqwertyuiopasdfghjklçzxcvbnm1234567890 ");
-        AG.setTaxaDeCrossover(0.6);
+		AG.setCaracteres("!,.:;?áÁãÃâÂõÕôÔóÓéêÉÊíQWERTYUIOPASDFGHJKLÇZXCVBNMqwertyuiopasdfghjklçzxcvbnm1234567890 ");
+		AG.setTaxaDeCrossover(0.6);
         AG.setTaxaDeMutacao(0.3);
         boolean eltismo = true;
+        int tamPop = 100;
+        int numMaxGeracoes = 10000;
         
-        int pop = in.nextInt();
-        while(pop < 0) {
-        	System.out.println("Valor de populacao invalido, precisa ser acima de 0");
-        	pop = in.nextInt();
-        }
-        int tamPop = pop;
-        //numero máximo de gerações
-        int geracoes = in.nextInt();
-        while(pop < 0) {
-        	System.out.println("Valor de geracoes invalido, precisa ser acima de 0");
-        	geracoes = in.nextInt();
-        }
-        int numMaxGeracoes = geracoes;
-
-        //define o número de genes do indivíduo baseado na solução
+        
+		Scanner in = new Scanner(System.in);
+		System.out.println("Defina a solução: ");
+		String solucao = in.nextLine();
+		while(solucao.length() < 5) {
+			System.out.println("Digite uma solução com tamanho maior que 5");
+			solucao = in.nextLine();
+		}
+        AG.setSolucao(solucao);      
         int numGenes = AG.getSolucao().length();
+        
 
         //cria a primeira população aleatérioa
         Populacao populacao = new Populacao(numGenes, tamPop);
@@ -41,7 +29,7 @@ public class Main {
         boolean temSolucao = false;
         int geracao = 0;
 
-        System.out.println("Fitness da solução: " + AG.getSolucao().length());
+        System.out.println("Iniciando... Fitness da solução: "+AG.getSolucao().length());
         
         //loop até o critério de parada
         while (!temSolucao && geracao < numMaxGeracoes) {
@@ -50,20 +38,24 @@ public class Main {
             //cria nova populacao
             populacao = AG.novaGeracao(populacao, eltismo);
 
-            System.out.println("Geração " + geracao + " | Fitness: " + populacao.getIndivduo(0).getFitnessInt() + " | Melhor: " + populacao.getIndivduo(0).getGenes());
+            System.out.println("Geração " + geracao + " | Fitness: " + populacao.getIndivduo(0).getFitness() + " | Melhor: " + populacao.getIndivduo(0).getGenes());
             
             //verifica se tem a solucao
             temSolucao = populacao.temSolocao(AG.getSolucao());
         }
 
         if (geracao == numMaxGeracoes) {
-            System.out.println("Número Maximo de Gerações | " + populacao.getIndivduo(0).getGenes() + " " + populacao.getIndivduo(0).getFitnessInt());
+            System.out.println("Número Maximo de Gerações | " + populacao.getIndivduo(0).getGenes() + " " + populacao.getIndivduo(0).getFitness());
         }
 
         if (temSolucao) {
-            System.out.println("Encontrado resultado na geração " + geracao + " | " + populacao.getIndivduo(0).getGenes() + " (Aptidão: " + populacao.getIndivduo(0).getFitnessInt() + ")");
+            System.out.println("Encontrado resultado na geração " + geracao + " | " + populacao.getIndivduo(0).getGenes() + " (Fitness: " + populacao.getIndivduo(0).getFitness() + ")");
         }
-
+    }
+	
+		
+		
+		
 	}
 
-}
+
--- AlgoritmoGenetico/src/Populacao.java
+++ AlgoritmoGenetico/src/Populacao.java
@@ -1,88 +1,88 @@
 
 public class Populacao {
-	private Individuo[] individuos;
-    private int tamPopulacao;
+	 private Individuo[] individuos;
+	    private int tamPopulacao;
 
-    //cria uma população com indivíduos aleatória
-    public Populacao(int numGenes, int tamPop) {
-        tamPopulacao = tamPop;
-        individuos = new Individuo[tamPop];
-        for (int i = 0; i < individuos.length; i++) {
-            individuos[i] = new Individuo(numGenes);
-        }
-    }
+	    //cria uma população com indivíduos aleatória
+	    public Populacao(int numGenes, int tamPop) {
+	        tamPopulacao = tamPop;
+	        individuos = new Individuo[tamPop];
+	        for (int i = 0; i < individuos.length; i++) {
+	            individuos[i] = new Individuo(numGenes);
+	        }
+	    }
 
-    //cria uma população com indivíduos sem valor, será composto posteriormente
-    public Populacao(int tamPop) {
-        tamPopulacao = tamPop;
-        individuos = new Individuo[tamPop];
-        for (int i = 0; i < individuos.length; i++) {
-            individuos[i] = null;
-        }
-    }
+	    //cria uma população com indivíduos sem valor, será composto posteriormente
+	    public Populacao(int tamPop) {
+	        tamPopulacao = tamPop;
+	        individuos = new Individuo[tamPop];
+	        for (int i = 0; i < individuos.length; i++) {
+	            individuos[i] = null;
+	        }
+	    }
 
-    //coloca um indivíduo em uma certa posição da população
-    public void setIndividuo(Individuo individuo, int posicao) {
-        individuos[posicao] = individuo;
-    }
+	    //coloca um indivíduo em uma certa posição da população
+	    public void setIndividuo(Individuo individuo, int posicao) {
+	        individuos[posicao] = individuo;
+	    }
 
-    //coloca um indivíduo na próxima posição disponível da população
-    public void setIndividuo(Individuo individuo) {
-        for (int i = 0; i < individuos.length; i++) {
-            if (individuos[i] == null) {
-                individuos[i] = individuo;
-                return;
-            }
-        }
-    }
+	    //coloca um indivíduo na próxima posição disponível da população
+	    public void setIndividuo(Individuo individuo) {
+	        for (int i = 0; i < individuos.length; i++) {
+	            if (individuos[i] == null) {
+	                individuos[i] = individuo;
+	                return;
+	            }
+	        }
+	    }
 
-    //verifoca se algum indivíduo da população possui a solução
-    public boolean temSolocao(String solucao) {
-        Individuo i = null;
-        for (int j = 0; j < individuos.length; j++) {
-            if (individuos[j].getGenes().equals(solucao)) {
-                i = individuos[j];
-                break;
-            }
-        }
-        if (i == null) {
-            return false;
-        }
-        return true;
-    }
+	    //verifoca se algum indivíduo da população possui a solução
+	    public boolean temSolocao(String solucao) {
+	        Individuo i = null;
+	        for (int j = 0; j < individuos.length; j++) {
+	            if (individuos[j].getGenes().equals(solucao)) {
+	                i = individuos[j];
+	                break;
+	            }
+	        }
+	        if (i == null) {
+	            return false;
+	        }
+	        return true;
+	    }
 
-    //ordena a população pelo valor de aptidão de cada indivíduo, do maior valor para o menor, assim se eu quiser obter o melhor indivíduo desta população, acesso a posição 0 do array de indivíduos
-    public void ordenaPopulacao() {
-        boolean trocou = true;
-        while (trocou) {
-            trocou = false;
-            for (int i = 0; i < individuos.length - 1; i++) {
-                if (individuos[i].getFitnessInt() < individuos[i + 1].getFitnessInt()) {
-                    Individuo temp = individuos[i];
-                    individuos[i] = individuos[i + 1];
-                    individuos[i + 1] = temp;
-                    trocou = true;
-                }
-            }
-        }
-    }
+	    //ordena a população pelo valor de aptidão de cada indivíduo, do maior valor para o menor, assim se eu quiser obter o melhor indivíduo desta população, acesso a posição 0 do array de indivíduos
+	    public void ordenaPopulacao() {
+	        boolean trocou = true;
+	        while (trocou) {
+	            trocou = false;
+	            for (int i = 0; i < individuos.length - 1; i++) {
+	                if (individuos[i].getFitness() < individuos[i + 1].getFitness()) {
+	                    Individuo temp = individuos[i];
+	                    individuos[i] = individuos[i + 1];
+	                    individuos[i + 1] = temp;
+	                    trocou = true;
+	                }
+	            }
+	        }
+	    }
 
-    //número de indivíduos existentes na população
-    public int getNumIndividuos() {
-        int num = 0;
-        for (int i = 0; i < individuos.length; i++) {
-            if (individuos[i] != null) {
-                num++;
-            }
-        }
-        return num;
-    }
+	    //número de indivíduos existentes na população
+	    public int getNumIndividuos() {
+	        int num = 0;
+	        for (int i = 0; i < individuos.length; i++) {
+	            if (individuos[i] != null) {
+	                num++;
+	            }
+	        }
+	        return num;
+	    }
 
-    public int getTamPopulacao() {
-        return tamPopulacao;
-    }
+	    public int getTamPopulacao() {
+	        return tamPopulacao;
+	    }
 
-    public Individuo getIndivduo(int pos) {
-        return individuos[pos];
-    }
+	    public Individuo getIndivduo(int pos) {
+	        return individuos[pos];
+	    }
 }
